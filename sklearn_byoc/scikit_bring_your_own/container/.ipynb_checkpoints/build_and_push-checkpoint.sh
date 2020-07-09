c8164f5ceb61e412244b298aa903d4cab431d1ca
EC2 Default User <ec2-user@ip-172-16-42-88.ec2.internal>
2020-07-09 23:45:52 +0000

    adjust text in notebook

--- sklearn_byoc/scikit_bring_your_own/container/.ipynb_checkpoints/build_and_push-checkpoint.sh
+++ sklearn_byoc/scikit_bring_your_own/container/.ipynb_checkpoints/build_and_push-checkpoint.sh
@@ -6,7 +6,7 @@
 # The argument to this script is the image name. This will be used as the image on the local
 # machine and combined with the account and region to form the repository name for ECR.
 
-cp -r ~/.aws container
+cp -r ~/.aws .
 
 image=$1
 

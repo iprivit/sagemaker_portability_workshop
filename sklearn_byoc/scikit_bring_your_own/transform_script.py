d4130ed70111b5c6c56aed3437527958431a474b
EC2 Default User <ec2-user@ip-172-16-42-88.ec2.internal>
2020-07-09 18:57:56 +0000

    update sklearn container to py3 part 2

--- sklearn_byoc/scikit_bring_your_own/transform_script.py
+++ sklearn_byoc/scikit_bring_your_own/transform_script.py
@@ -3,8 +3,6 @@ import tarfile
 import os
 
 def model_fn(model_dir):
-#     with tarfile.open(f'{model_dir}/decision-tree-model.tar.gz','r:gz') as tar:
-#         tar.extractall()
-    print(os.system(f'ls {model_dir}'))
+#     print(os.system(f'ls {model_dir}'))
     model = pickle.load(open(f'{model_dir}/decision-tree-model.pkl','rb'))
     return model
